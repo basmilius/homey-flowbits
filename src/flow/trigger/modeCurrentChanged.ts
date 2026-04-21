1882898c3bef503e46659b3d8d051aa7ca21d5c0
Bas Milius <basmilius@hotmail.com>
2026-04-21 15:07:54 +0200

    fix(trigger): add typings for mode tokens.

--- src/flow/trigger/modeCurrentChanged.ts
+++ src/flow/trigger/modeCurrentChanged.ts
@@ -2,8 +2,12 @@ import { FlowTriggerEntity, trigger } from '@basmilius/homey-common';
 import type { FlowBitsApp } from '../../types';
 
 @trigger('mode_current_changed')
-export default class extends FlowTriggerEntity<FlowBitsApp> {
+export default class extends FlowTriggerEntity<FlowBitsApp, unknown, unknown, Tokens> {
     async onRun(): Promise<boolean> {
         return true;
     }
 }
+
+type Tokens = {
+    readonly mode: string;
+};
